64dea1a2f6db66e41bfba7a5e1f8890a709ad73e
Julian Goldsmith <julian@juliangoldsmith.com>
2017-08-22 22:05:31 -0400

    get accelerators working

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,17 @@ use user32::*;
 use winapi::*;
 use main_window::{MainWindow, MAIN_WINDOW_INSTANCE};
 
+const IDA_ACCEL_TABLE: i32 = 10000;
+
+// HACK: this will be in the new version of winapi-rs
+extern "system" {
+    pub fn TranslateAcceleratorW(
+        hWnd: HWND,
+        hAccTable: HACCEL,
+        lpMsg: LPMSG,
+    ) -> c_int;
+}
+
 fn main() {
 	unsafe {
 		let icc = INITCOMMONCONTROLSEX {
@@ -30,12 +41,26 @@ fn main() {
 		UpdateWindow(MAIN_WINDOW_INSTANCE.hwnd);
 	};
 	
+	let accel = unsafe {
+		LoadAcceleratorsW(MAIN_WINDOW_INSTANCE.hinstance, IDA_ACCEL_TABLE as WORD as ULONG_PTR as LPWSTR)
+	};
+	
+	if accel == ptr::null_mut() {
+		let error = unsafe {
+			kernel32::GetLastError()
+		};
+		
+		panic!("Couldn't create accel: {:?}", error);
+	};
+		
 	unsafe {
 		let mut msg: MSG = mem::uninitialized();
 		
 		while GetMessageW(&mut msg, ptr::null_mut(), 0, 0) > 0 {
-			TranslateMessage(&msg);
-			DispatchMessageW(&msg);
+			if TranslateAcceleratorW(MAIN_WINDOW_INSTANCE.hwnd, accel, &mut msg) == 0 {
+				TranslateMessage(&msg);
+				DispatchMessageW(&msg);
+			};
 		};
 	};
 }
